9178085305b186b9742c27a83ebc2b9ae153e29d
Valerio Corso <corso.valerio@gmail.com>
2024-06-21 18:14:50 +0200

    some classes, wip

---
+++ Assets/Project/Editor/JSONToScriptableObjEditor.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class JSONToScriptableObjEditor : EditorWindow
+{
+    private string jsonFilePath = "Assets/Content/Sample/sample.json";
+    private string outputPath = "Assets/Content/Sample";
+    
+    [MenuItem("Tools/Scriptable Object Maker")]
+    public static void ShowWindow()
+    {
+        GetWindow<JSONToScriptableObjEditor>("GameData Editor");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("GameData Editor", EditorStyles.boldLabel);
+
+        TextAsset p = AssetDatabase.LoadAssetAtPath<TextAsset>(jsonFilePath);
+        TextAsset ph = AssetDatabase.LoadAssetAtPath<TextAsset>("Content/Sample/sample.json");
+
+        // jsonFilePath = EditorGUILayout.TextField("JSON File Path", jsonFilePath);
+        // outputPath = EditorGUILayout.TextField("Output Path", outputPath);
+
+        if (GUILayout.Button("Generate C# Classes"))
+        {
+            GenerateCSharpClasses();
+        }
+
+        // if (GUILayout.Button("Create ScriptableObject"))
+        // {
+        //     CreateScriptableObjectFromJson();
+        // }
+    }
+
+    private void GenerateCSharpClasses()
+    {
+        if (string.IsNullOrEmpty(jsonFilePath) || !File.Exists(jsonFilePath))
+        {
+            Debug.LogError("Invalid JSON file path");
+            return;
+        }
+
+        string json = File.ReadAllText(jsonFilePath);
+        JsonCodeGenerator.GenerateClasses(json, outputPath);
+        JsonCodeGenerator.GenerateScriptableObjectClass(outputPath, "Root");
+
+        AssetDatabase.Refresh();
+        Debug.Log("C# classes generated successfully!");
+    }
+
+    // private void CreateScriptableObjectFromJson()
+    // {
+    //     if (string.IsNullOrEmpty(jsonFilePath) || !File.Exists(jsonFilePath))
+    //     {
+    //         Debug.LogError("Invalid JSON file path");
+    //         return;
+    //     }
+    //
+    //     string json = File.ReadAllText(jsonFilePath);
+    //     var gameData = JsonCodeGenerator.LoadJson<RootScriptableObject>(jsonFilePath);
+    //     if (gameData != null)
+    //     {
+    //         string assetPath = "Assets/GameData.asset";
+    //         AssetDatabase.CreateAsset(gameData, assetPath);
+    //         AssetDatabase.SaveAssets();
+    //         AssetDatabase.Refresh();
+    //         EditorUtility.FocusProjectWindow();
+    //         Selection.activeObject = gameData;
+    //
+    //         Debug.Log("ScriptableObject created successfully!");
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("Failed to create ScriptableObject");
+    //     }
+    // }
+}
---
+++ Assets/Project/Editor/JsonCodeGenerator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Unity.Plastic.Newtonsoft.Json.Linq;
+
+public static class JsonCodeGenerator
+{
+    public static void GenerateClasses(string json, string outputPath)
+    {
+        var jObject = JObject.Parse(json);
+        var classDefinitions = new Dictionary<string, StringBuilder>();
+        GenerateClass("Root", jObject, classDefinitions);
+
+        foreach (var classDef in classDefinitions)
+        {
+            var filePath = Path.Combine(outputPath, $"{classDef.Key}.cs");
+            File.WriteAllText(filePath, classDef.Value.ToString());
+        }
+    }
+    
+    public static void GenerateScriptableObjectClass(string outputPath, string rootClassName)
+    {
+        var classBuilder = new StringBuilder();
+        classBuilder.AppendLine("using UnityEngine;");
+        classBuilder.AppendLine("[CreateAssetMenu(fileName = \"GameData\", menuName = \"ScriptableObjects/GameData\", order = 1)]");
+        classBuilder.AppendLine($"public class {rootClassName}ScriptableObject : ScriptableObject");
+        classBuilder.AppendLine("{");
+        classBuilder.AppendLine($"    public {rootClassName} Data;");
+        classBuilder.AppendLine("}");
+
+        string filePath = Path.Combine(outputPath, $"{rootClassName}ScriptableObject.cs");
+        File.WriteAllText(filePath, classBuilder.ToString());
+    }
+
+
+    private static void GenerateClass(string className, JObject jObject, IDictionary<string, StringBuilder> classDefinitions)
+    {
+        var classBuilder = new StringBuilder();
+        classBuilder.AppendLine("using System;");
+        classBuilder.AppendLine("using UnityEngine;");
+        classBuilder.AppendLine("[System.Serializable]");
+        classBuilder.AppendLine($"public class {className}");
+        classBuilder.AppendLine("{");
+
+        foreach (var property in jObject.Properties())
+        {
+            string propName = property.Name;
+            JToken propValue = property.Value;
+
+            string propType;
+            if (propValue.Type == JTokenType.Object)
+            {
+                propType = ToPascalCase(propName);
+                GenerateClass(propType, (JObject)propValue, classDefinitions);
+            }
+            else if (propValue.Type == JTokenType.Array)
+            {
+                var array = (JArray)propValue;
+                if (array.Count > 0 && array[0].Type == JTokenType.Object)
+                {
+                    propType = ToPascalCase(propName) + "[]";
+                    GenerateClass(ToPascalCase(propName), (JObject)array[0], classDefinitions);
+                }
+                else
+                {
+                    propType = "object[]";
+                }
+            }
+            else
+            {
+                propType = GetCSharpType(propValue.Type);
+            }
+
+            classBuilder.AppendLine($"    public {propType} {ToPascalCase(propName)};");
+        }
+
+        classBuilder.AppendLine("}");
+        classDefinitions[className] = classBuilder;
+    }
+
+    private static string GetCSharpType(JTokenType jsonType)
+    {
+        switch (jsonType)
+        {
+            case JTokenType.Integer:
+                return "int";
+            case JTokenType.Float:
+                return "float";
+            case JTokenType.String:
+                return "string";
+            case JTokenType.Boolean:
+                return "bool";
+            default:
+                return "string";
+        }
+    }
+
+    private static string ToPascalCase(string str)
+    {
+        return char.ToUpper(str[0]) + str.Substring(1);
+    }
+}
